0c288e7208d377f1421cb748c33fbe1c0b1d806d
AjayKrishna05 <krishna.ajay.ajay10@gmail.com>
2021-10-21 12:02:35 +0530

    ngFor Demonstration

---
+++ ngFor/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ngFor';
+  technologies = ["HTML","CSS","BOOTSTRAP","JAVASCRIPT","JQUERY","TYPESCRIPT","ANGULAR"];
+}
